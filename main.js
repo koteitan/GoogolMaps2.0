ff386ab0e5e79fca298a1cfeed1baea80ef5930a
koteitan <ukaibutton@mac.com>
2019-06-01 00:52:18 -0400

    parse sheet and make text.

--- main.js
+++ main.js
@@ -1,6 +1,27 @@
+var Entry=function(line){
+  var col = line.content.$t.split(",");
+  this.name  = col[0].split(": ")[1];
+  this.year  = col[1].split(": ")[1];
+  this.order = col[2].split(": ")[1];
+}
+Entry.prototype.toString = function(){
+  return this.year + ":" + this.name + "(" + this.order + ")";
+}
 var expand=function(res){
-  var entries = res.feed.entry.length;
-  document.getElementById("debug").innerHTML=entries;
+  var sheet = res.feed.entry;
+  var entries = sheet.length;
+  var entrylist = [];
+  for(var e=0;e<entries;e++){
+    var entry = new Entry(sheet[e]);
+    entrylist.push(entry);
+  }
+
+  var str="";
+  for(var e=0;e<entries;e++){
+    str = str + entrylist[e].toString();
+    str = str + "\n";
+  }
+  document.getElementById("debug").innerHTML = str;
 }
 
 var entry=function(){
