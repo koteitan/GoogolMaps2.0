bd9ed746be45eefc77710063007f70465f9503b0
koteitan <ukaibutton@mac.com>
2019-06-01 00:30:31 -0400

    load sheet and output as textarea

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+var expand=function(res){
+  var entries = res.feed.entry.length;
+  document.getElementById("debug").innerHTML=entries;
+}
+
 var entry=function(){
   var spreadsheetId = "11WH6PrhFAcdMEWSTjxSjZ7_rWHg-b8shAvSFn99bdyQ",
     url = "https://spreadsheets.google.com/feeds/list/" +
@@ -6,7 +11,7 @@ var entry=function(){
   $.get({
     url: url,
     success: function(response) {
-      console.log(response);
+      expand(response);
     }
   });
 };
