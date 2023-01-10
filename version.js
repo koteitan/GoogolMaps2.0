f91c9cfdc6a5a09c43b2e51cc20533e2b9ee0a8b
koteitan <ukaibutton@mac.com>
2023-01-11 01:59:50 +0900

    remove unused elements

--- version.js
+++ version.js
@@ -1,10 +1,1 @@
 document.getElementById("versiondisplay").innerHTML = "0.3";
-document.getElementById("changelog").innerHTML = 
-"<ul>"+
-"<li>2018/05/31 trial for parsing sheet.</li>"+
-"<li>2018/06/11 start implementation of Maps.</li>"+
-"</ul>";
-document.getElementById("knownbugs").innerHTML = 
-"<ul>"+
-"<li>under construction</li>"+
-"</ul>";
